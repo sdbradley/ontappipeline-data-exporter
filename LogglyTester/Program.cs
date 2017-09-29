64b2db7d99e559ee4fb6a9b62c1f31c6287f49a3
coreinsightgroup <scott@coreinsightgroup.com>
2017-09-28 20:10:37 -0500

    committing changes to master

---
+++ LogglyTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogglyTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog logger = LogManager.GetLogger(typeof(Program));
+            logger.Error("******AAA***AAA***AAA********");
+            Console.Read();
+        }
+    }
+}
